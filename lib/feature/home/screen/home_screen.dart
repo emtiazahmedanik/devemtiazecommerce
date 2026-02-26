da53017132b6a57c7675cd0b8181b4727aef6810
emtiaz <emtiazahmedanik4@gmail.com>
2026-02-27 01:05:28 +0600

    refresh done

--- lib/feature/home/screen/home_screen.dart
+++ lib/feature/home/screen/home_screen.dart
@@ -22,126 +22,122 @@ class HomeScreen extends StatelessWidget {
             if (homeController.isLoading.value) {
               return const Center(child: CircularProgressIndicator());
             }
-            return RefreshIndicator(
-              onRefresh: homeController.fetchProducts,
-              notificationPredicate: (notification) => notification.depth == 0,
-              child: NestedScrollView(
-                physics: const AlwaysScrollableScrollPhysics(),
-                headerSliverBuilder: (context, innerBoxIsScrolled) {
-                  return [
-                    SliverAppBar(
-                      title: const Text('Home'),
-                      floating: false,
-                      pinned: false,
-                    ),
-                    SliverPadding(
-                      padding: const EdgeInsets.only(bottom: 16),
-                      sliver: SliverToBoxAdapter(
-                        child: SizedBox(
-                          height: 280,
-                          child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            padding: const EdgeInsets.symmetric(horizontal: 16),
-                            itemCount: homeController.productList.length > 5
-                                ? 5
-                                : homeController.productList.length,
-                            itemBuilder: (context, index) {
-                              return Container(
-                                width: 180,
-                                margin: const EdgeInsets.only(right: 12),
-                                decoration: BoxDecoration(
-                                  color: Colors.grey[300],
-                                  borderRadius: BorderRadius.circular(12),
-                                ),
-                                child: ProductCard(
-                                  product: homeController.productList[index],
-                                ),
-                              );
-                            },
-                          ),
+            return NestedScrollView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              headerSliverBuilder: (context, innerBoxIsScrolled) {
+                return [
+                  SliverAppBar(
+                    title: const Text('Home'),
+                    floating: false,
+                    pinned: false,
+                  ),
+                  SliverPadding(
+                    padding: const EdgeInsets.only(bottom: 16),
+                    sliver: SliverToBoxAdapter(
+                      child: SizedBox(
+                        height: 280,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          itemCount: homeController.productList.length > 5
+                              ? 5
+                              : homeController.productList.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              width: 180,
+                              margin: const EdgeInsets.only(right: 12),
+                              decoration: BoxDecoration(
+                                color: Colors.grey[300],
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              child: ProductCard(
+                                product: homeController.productList[index],
+                              ),
+                            );
+                          },
                         ),
                       ),
                     ),
-                    SliverPersistentHeader(
-                      pinned: true,
-                      delegate: StickyTabDelegate(
-                        TabBar(
-                          controller: homeController.tabController,
-                          isScrollable: true,
-                          physics: const ClampingScrollPhysics(),
-                          tabAlignment: TabAlignment.start,
-                          indicatorColor: Colors.redAccent,
-                          labelColor: Colors.redAccent,
-                          dividerColor: Colors.transparent,
-                          unselectedLabelColor: Colors.black.withValues(
-                            alpha: 0.6,
-                          ),
-                          indicator: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            color: Colors.redAccent.withValues(alpha: 0.1),
-                          ),
-
-                          tabs: const [
-                            IconTextTab(
-                              text: "Trending",
-                              icon: FontAwesomeIcons.fire,
-                            ),
-                            IconTextTab(
-                              text: "Flash Sale",
-                              icon: FontAwesomeIcons.bolt,
-                            ),
-                            IconTextTab(
-                              text: "Fashion",
-                              icon: FontAwesomeIcons.accusoft,
-                            ),
-                            IconTextTab(
-                              text: "Best Deals",
-                              icon: FontAwesomeIcons.tags,
-                            ),
-                            IconTextTab(
-                              text: "Hot Offers",
-                              icon: FontAwesomeIcons.hotjar,
-                            ),
-                            IconTextTab(
-                              text: "For You",
-                              icon: FontAwesomeIcons.heartCircleBolt,
-                            ),
-                          ],
+                  ),
+                  SliverPersistentHeader(
+                    pinned: true,
+                    delegate: StickyTabDelegate(
+                      TabBar(
+                        controller: homeController.tabController,
+                        isScrollable: true,
+                        physics: const ClampingScrollPhysics(),
+                        tabAlignment: TabAlignment.start,
+                        indicatorColor: Colors.redAccent,
+                        labelColor: Colors.redAccent,
+                        dividerColor: Colors.transparent,
+                        unselectedLabelColor: Colors.black.withValues(
+                          alpha: 0.6,
                         ),
+                        indicator: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Colors.redAccent.withValues(alpha: 0.1),
+                        ),
+            
+                        tabs: const [
+                          IconTextTab(
+                            text: "Trending",
+                            icon: FontAwesomeIcons.fire,
+                          ),
+                          IconTextTab(
+                            text: "Flash Sale",
+                            icon: FontAwesomeIcons.bolt,
+                          ),
+                          IconTextTab(
+                            text: "Fashion",
+                            icon: FontAwesomeIcons.accusoft,
+                          ),
+                          IconTextTab(
+                            text: "Best Deals",
+                            icon: FontAwesomeIcons.tags,
+                          ),
+                          IconTextTab(
+                            text: "Hot Offers",
+                            icon: FontAwesomeIcons.hotjar,
+                          ),
+                          IconTextTab(
+                            text: "For You",
+                            icon: FontAwesomeIcons.heartCircleBolt,
+                          ),
+                        ],
                       ),
                     ),
-                  ];
-                },
-                body: TabBarView(
-                  controller: homeController.tabController,
-                  physics: const PageScrollPhysics(),
-                  children: [
-                    ProductList(
-                      tabKey: "trending",
-                      homeController: homeController,
-                    ),
-                    ProductList(
-                      tabKey: "flash_sale",
-                      homeController: homeController,
-                    ),
-                    ProductList(
-                      tabKey: "fashion",
-                      homeController: homeController,
-                    ),
-                    ProductList(
-                      tabKey: "best_deals",
-                      homeController: homeController,
-                    ),
-                    ProductList(
-                      tabKey: "hot_offers",
-                      homeController: homeController,
-                    ),
-                    ProductList(
-                      tabKey: "for_you",
-                      homeController: homeController,
-                    ),
-                  ],
-                ),
+                  ),
+                ];
+              },
+              body: TabBarView(
+                controller: homeController.tabController,
+                physics: const PageScrollPhysics(),
+                children: [
+                  ProductList(
+                    tabKey: "trending",
+                    homeController: homeController,
+                  ),
+                  ProductList(
+                    tabKey: "flash_sale",
+                    homeController: homeController,
+                  ),
+                  ProductList(
+                    tabKey: "fashion",
+                    homeController: homeController,
+                  ),
+                  ProductList(
+                    tabKey: "best_deals",
+                    homeController: homeController,
+                  ),
+                  ProductList(
+                    tabKey: "hot_offers",
+                    homeController: homeController,
+                  ),
+                  ProductList(
+                    tabKey: "for_you",
+                    homeController: homeController,
+                  ),
+                ],
               ),
             );
           }),
